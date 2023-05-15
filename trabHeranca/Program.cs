2156f09e4ccf57e9175b55458fd4e7b5dbf94a0b
Lucas Cupertino <olucas.cdev@gmail.com>
2023-05-15 20:22:09 -0300

    comit

---
+++ trabHeranca/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Classe base
+public class Animal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void MakeSound()
+    {
+        Console.WriteLine("O animal fez um som");
+    }
+}
+
+// Classe derivada (subclasse)
+public class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("O cachorro latiu.");
+    }
+}
+
+// Classe principal
+class Program
+{
+    static void Main()
+    {
+        // Cria um objeto da classe base
+        Animal animal = new Animal();
+        animal.Name = "Leão";
+        animal.Age = 5;
+        animal.MakeSound();
+
+        // Cria um objeto da classe derivada
+        Dog dog = new Dog();
+        dog.Name = "Bastião";
+        dog.Age = 3;
+        dog.MakeSound();
+        dog.Bark();
+    }
+}
